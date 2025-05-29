f1979b2f3bd2c9a1cdf407bcbcd2fed239980630
LePhanHaiLong <longlph.23it@gmail.com>
2025-05-29 23:15:48 +0700

    fetch wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,29 @@
 import MainSlider from "@/components/main/main.slider";
+import { sendRequestJS } from "@/utils/old.api";
 import { Container } from "@mui/material";
-export default function HomePage() {
+export default async function HomePage() {
+
+  // const response = await fetch("htts", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     category: "chill",
+  //     limit: 10,
+  //   })
+  // })
+  // console.log(await response.json());
+
+  // const response = await sendRequestJS({
+  //   url: "https://api.example.com/data",
+  //   method: "POST",
+  //   body: {
+  //     category: "CHILL",
+  //     limit: 10,
+  //   },
+  // })
+  // console.log(response);
   return (
     <Container>
       <MainSlider />
--- src/components/footer/app.footer.tsx
+++ src/components/footer/app.footer.tsx
@@ -2,7 +2,6 @@
 
 import useHasMounted from "@/utils/customHook";
 import { AppBar, Container } from "@mui/material";
-import shadows from "@mui/material/styles/shadows";
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
@@ -22,22 +21,22 @@ const AppFooter = () => {
             <Container sx={{ display: 'flex', gap: 10 }}>
                 <AudioPlayer
                     autoPlay
-                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/audio.mp3`}
                     volume={0.5}
                     style={{
                         boxShadow: 'unset',
-                        background:'#f2f2f2'
+                        background: '#f2f2f2'
                     }}
                 />
                 <div style={{
-                    display:'flex',
-                    flexDirection:'column',
-                    alignItems:'start',
-                    justifyContent:'center',
-                    minWidth:'100px'
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'start',
+                    justifyContent: 'center',
+                    minWidth: '100px'
                 }}>
-                    <div style={{color:'#ccc'}}>Long Le</div>
-                    <div style={{color:'black'}}>Hi</div>
+                    <div style={{ color: '#ccc' }}>Long Le</div>
+                    <div style={{ color: 'black' }}>Hi</div>
                 </div>
             </Container>
         </AppBar>
